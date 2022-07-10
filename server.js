1d5367a4115c467c6c646304d0e507bdf291f2d3
zinx110 <tasdid110@gmail.com>
2022-07-11 02:16:36 +0600

    dotenv usage

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ const express = require("express");
 
 const app = express();
 
+require("dotenv").config();
+
+// default most basic route
 app.get("/", (req, res) => {
-  res.send("Hi");
+  res.send(process.env.SECRET);
+  //   res.json("hi");
 });
 
-app.listen(3000);
+//
+console.log(process.env.SECRET);
+
+// start server at 3000
+app.listen(3000, () => console.log("server is running"));
